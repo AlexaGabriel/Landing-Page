a89d8529f726a2ddf5cb50727c579fd6c315420a
AlexaGabriel <alexandre181828@gmail.com>
2023-10-18 21:48:00 -0300

    atl

--- src/App.jsx
+++ src/App.jsx
@@ -60,17 +60,17 @@ function App() {
           <section>   
             <article className='editor'>
               <h2>Designed for the future</h2>
-              <h3> Introducing an extensible editor</h3>
-              <p>Blogr features an exceedingly intuitive interface which lets you focus on one thing: creating content.
+              <h3 className='linha1'> Introducing an extensible editor</h3>
+              <p className='linha1'>Blogr features an exceedingly intuitive interface which lets you focus on one thing: creating content.
               The editor supports management of multiple blogs and allows easy manipulation of embeds such as images,
               videos, and Markdown. Extensibility with plugins and themes provide easy ways to add functionality or
               change the looks of a blog.</p>
-              <h3>Robust content management</h3>
-              <p>Flexible content management enables users to easily move through posts. Increase the usability of your blog
+              <h3 className='linha2'>Robust content management</h3>
+              <p className='linha2'>Flexible content management enables users to easily move through posts. Increase the usability of your blog
               by adding customized categories, sections, format, or flow. With this functionality, you’re in full control.
               </p>
               <figure>
-                <img src={editor} width= "250px" height="250px" alt="drawed page" />
+                <img src={editor} width= "250px" height="250px" alt="drawed page" className='page' />
               </figure>
             </article>
             <article>
